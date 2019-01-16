162698293894c5e56bc47b976a59172c409da3b0
watcharapon kestrachon <thecvit2328@gmail.com>
2019-01-16 15:02:36 +0700

    omise.charges

--- app/controllers/omise/index.js
+++ app/controllers/omise/index.js
@@ -2,19 +2,20 @@ import { omise } from '../../libs'
 
 const omiseService = {
   charge: (req, res) => {
-    const { omiseCustomerId, amount } = req.body
+    const { omiseCustomerId, omiseCardId, amount } = req.body
     omise.charges.create(
       {
         amount,
         currency: 'thb',
-        customer: omiseCustomerId
+        customer: omiseCustomerId,
+        card: omiseCardId
       },
       function(error, response) {
         if (error) res.send(error)
         /* Response. */
         try {
           const { id, status, amount } = response
-          res.status(201).json({ id, status, amount })
+          res.status(200).json({ id, status, amount })
         } catch (error) {
           res.json(error)
         }
